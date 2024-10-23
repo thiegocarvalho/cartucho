5675b7e52562dbe7866fb1a36d21bbfd4365ad28
THIEGO CARVALHO NASCIMENTO <thiego@rocketmail.com>
2024-10-23 19:41:06 -0300

    fix express and hide ads

--- index.js
+++ index.js
@@ -4,24 +4,14 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
-app.use('/src/EmulatorJS', express.static(path.join(__dirname, 'src/EmulatorJS')));
-app.use('/node_modules/flyonui', express.static(path.join(__dirname, 'node_modules/flyonui')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/emulator', express.static(path.join(__dirname, 'public/emulator')));
+app.use('/flyonui', express.static(path.join(__dirname, 'public/flyonui')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 
-app.get('/', (req, res) => {
-    const data = { };
-    res.render('index', data);
-});
-
-app.get('/how-its-works', (req, res) => {
-    res.render('how-its-works');
-});
-
-app.get('/play', (req, res) => {
-    const { platform, cid } = req.query;
-    const data = { platform: platform, cid: cid};
-    res.render('play', data);
+app.get(['/', '/play.html', '/how-its-works.html'], (req, res) => {
+    const page = req.path === '/' ? 'index.html' : req.path.slice(1);
+    res.sendFile(path.join(__dirname, 'public', page));
 });
 
 const port = process.env.PORT || 3000;
